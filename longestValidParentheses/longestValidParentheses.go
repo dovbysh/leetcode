6bb5002497bf9944270e480a28fc6672098a131a
Andrey Dovbyshko <dovbysh@gmail.com>
2022-09-19 22:16:01 +0300

    work

--- longestValidParentheses/longestValidParentheses.go
+++ longestValidParentheses/longestValidParentheses.go
@@ -1,13 +1,32 @@
 package longestValidParentheses
 
 func longestValidParentheses(s string) int {
+	if check([]byte(s), 0, len(s)-1) {
+		return len(s)
+	}
+	lens := len(s)
+	maxi := lens - 2
+	if maxi < 1 {
+		return 0
+	}
+	b := []byte(s)
+	for i := 1; i <= maxi; i++ {
+		if (lens-i)%2 == 1 {
+			continue
+		}
+		for j := 0; j <= i; j++ {
+			if check(b, i-j, lens-i-1) {
+				return lens - i
+			}
+		}
+	}
 	return 0
 }
 
-func check(zz string) bool {
+func check(zz []byte, first, length int) bool {
 	c := 0
-	for _, r := range zz {
-		switch r {
+	for i := first; i <= first+length; i++ {
+		switch zz[i] {
 		case '(':
 			c++
 			break
@@ -16,6 +35,9 @@ func check(zz string) bool {
 			break
 		}
 		if c < 0 {
+			return false
+		}
+		if c > (first + length - i + 3) {
 			return false
 		}
 	}
